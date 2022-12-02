f68c3bdbe27172d5b945871bfd9a438457df43e6
Guilherme Galarça Dutra <udutra@gmail.com>
2022-12-02 01:05:43 -0300

    Desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,14 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
+            if (Suite.Capacidade >= hospedes.Count) {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte.");
             }
         }
 
@@ -37,24 +37,17 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
+            
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
-
-            return valor;
+            return DiasReservados >= 10 ? (Suite.ValorDiaria * DiasReservados) - (Suite.ValorDiaria * DiasReservados * 0.10M) : (Suite.ValorDiaria * DiasReservados);
         }
     }
 }
